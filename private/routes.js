5e852fe696d089fcc30f701c7369d0b5d03a55e6
DJFriar <tommy.craft@icloud.com>
2021-02-04 22:56:23 -0600

    added 404 page

--- private/routes.js
+++ private/routes.js
@@ -52,6 +52,6 @@ module.exports = function (app) {
 
   // If no matching route is found default to home
   app.get("*", function (req, res) {
-    res.render("pages/index");
+    res.render("pages/404");
   });
 };
